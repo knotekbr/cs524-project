62419653f912f14e7975e6b580dfa5cfabd9197b
knotekbr <knotekbr@gmail.com>
2023-10-08 10:55:15 -0400

    add NestedNavBar to admin base page

--- frontend/src/pages/admin/AdminBasePage/AdminBasePage.tsx
+++ frontend/src/pages/admin/AdminBasePage/AdminBasePage.tsx
@@ -1,13 +1,17 @@
-import Typography from "@mui/material/Typography";
-
 import { Outlet } from "react-router-dom";
 
+import { NestedNavBar, NestedNavBarProps } from "~components/layout/NestedNavBar";
 import { PageWrapper } from "~components/layout/PageWrapper";
+
+const adminNavLinks: NestedNavBarProps["links"] = [
+  { label: "Question Management", to: "questions" },
+  { label: "Game Management", to: "games" },
+];
 
 export default function AdminBasePage() {
   return (
     <PageWrapper>
-      <Typography>Admin - Wrapper</Typography>
+      <NestedNavBar links={adminNavLinks} />
       <Outlet />
     </PageWrapper>
   );
