222345a3050e5720b612f45d1d65a61c3c27d70a
knotekbr <70414881+knotekbr@users.noreply.github.com>
2023-11-21 11:26:42 -0500

    relocate auth strategies and modify JWT strategy behavior (#6)

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,10 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UsersModule } from "src/users/users.module";
 import { PassportModule } from "@nestjs/passport";
-import { LocalStrategy } from "../common/strategies/local.strategy";
+import { LocalStrategy } from "./strategies/local.strategy";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtSecret } from "src/constants";
-import { JwtStrategy } from "../common/strategies/jwt.strategy";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
   imports: [
