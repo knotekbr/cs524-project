4b240fa95cbf5e3b3ebb3399d347a3d1219c81c5
knotekbr <knotekbr@gmail.com>
2023-11-28 17:16:07 -0500

    convert login/signup pages to controlled forms

--- frontend/src/pages/auth/LogInPage/LogInPage.tsx
+++ frontend/src/pages/auth/LogInPage/LogInPage.tsx
@@ -4,26 +4,60 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
-import { NavLink } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { NavLink, useNavigate } from "react-router-dom";
 
+import { useLogInMutation } from "~api/request/auth";
+import { ControlledTextField } from "~components/forms/ControlledTextField";
 import { PageWrapper } from "~components/layout/PageWrapper";
+import { useFeedback, snackbarOptions } from "~components/utility/FeedbackProvider";
+
+import { loginSchema, loginSchemaDefaults } from "./loginSchema";
+
+import type { LoginDto } from "~types";
+
+import type { SubmitErrorHandler, SubmitHandler } from "react-hook-form";
 
 export default function LogInPage() {
+  const navigate = useNavigate();
+  const { showSnackbar } = useFeedback();
+  const [logIn, { isLoading }] = useLogInMutation();
+
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(loginSchema),
+    mode: "onChange",
+    defaultValues: loginSchemaDefaults(),
+  });
+
+  const onSubmit: SubmitHandler<LoginDto> = (data) => {
+    logIn({ bodyParams: data })
+      .unwrap()
+      .then(() => {
+        navigate("/");
+      });
+  };
+
+  const onError: SubmitErrorHandler<LoginDto> = () => {
+    showSnackbar(snackbarOptions.formErrors());
+  };
+
   return (
     <PageWrapper alignItems="center" gap={1}>
       <Card sx={{ width: 0.5 }}>
         <CardHeader title="Log In" />
         <CardContent>
           <Stack gap={1} mt={-3}>
-            <TextField label="Email Address" />
-            <TextField label="Password" />
+            <ControlledTextField control={control} name="email" label="Email Address" />
+            <ControlledTextField control={control} name="password" label="Password" type="password" />
           </Stack>
         </CardContent>
         <CardActions sx={{ justifyContent: "flex-end" }}>
-          <Button>Log In</Button>
+          <Button onClick={handleSubmit(onSubmit, onError)} disabled={isLoading}>
+            Log In
+          </Button>
         </CardActions>
       </Card>
       <NavLink to="/signup" style={{ textDecoration: "none" }}>
