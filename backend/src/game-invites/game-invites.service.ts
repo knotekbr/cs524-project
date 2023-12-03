5dad44399b71e71c695d08008abbb7f4a260ae25
knotekbr <knotekbr@gmail.com>
2023-12-03 09:54:39 -0500

    add GameInvitesService

--- backend/src/game-invites/game-invites.service.ts
+++ backend/src/game-invites/game-invites.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { GameInvite, Prisma } from "@prisma/client";
 
 @Injectable()
-export class GameInvitesService {}
+export class GameInvitesService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(gameInviteWhereUniqueInput: Prisma.GameInviteWhereUniqueInput): Promise<GameInvite | null> {
+    return this.prisma.gameInvite.findUnique({
+      where: gameInviteWhereUniqueInput,
+    });
+  }
+
+  async findMany(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.GameInviteWhereUniqueInput;
+    where?: Prisma.GameInviteWhereInput;
+    orderBy?: Prisma.GameInviteOrderByWithRelationInput;
+  }): Promise<GameInvite[]> {
+    const { cursor, orderBy, skip, take, where } = params;
+    return this.prisma.gameInvite.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async create(data: Prisma.GameInviteCreateInput): Promise<GameInvite> {
+    return this.prisma.gameInvite.create({ data });
+  }
+
+  async update(params: {
+    where: Prisma.GameInviteWhereUniqueInput;
+    data: Prisma.GameInviteUpdateInput;
+  }): Promise<GameInvite> {
+    const { data, where } = params;
+    return this.prisma.gameInvite.update({
+      data,
+      where,
+    });
+  }
+
+  async delete(where: Prisma.GameInviteWhereUniqueInput): Promise<GameInvite> {
+    return this.prisma.gameInvite.delete({ where });
+  }
+}
