7c590d645081cd7fbac7540011eeb4cc66a67799
Ragi Unnithan <23237905+ragiunnithan@users.noreply.github.com>
2023-12-03 08:36:51 -0500

    Made changes to Users Controller file (#11)
    * Made changes to Users Controller file
    
    * fix naming conflict
    
    ---------
    
    Co-authored-by: knotekbr <knotekbr@gmail.com>

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Patch, Request } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { Auth } from "src/common/decorators/auth.decorator";
+import { UpdateProfileDto } from "./dtos/UpdateProfileDto";
+import * as bcrypt from "bcrypt";
 
 @Controller("users")
-export class UsersController {}
+@Auth()
+export class UsersController {
+  constructor(private usersService: UsersService) {} //user defined functio in users.service.ts
+
+  @Get("profle")
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Patch("profile")
+  async updateProfile(@Request() req, @Body() body: Partial<UpdateProfileDto>) {
+    const { nickname, password, newPassword } = body;
+    const id: number = req.user.id;
+
+    let newSaltedPassword: string | undefined = undefined;
+
+    if (password && newPassword) {
+      const user = await this.usersService.findOne({ id });
+
+      if (user && (await bcrypt.compare(password, user.saltedPassword))) {
+        newSaltedPassword = await bcrypt.hash(newPassword, 10);
+      }
+    }
+
+    // eslint-disable-next-line
+    const { saltedPassword, ...result } = await this.usersService.update({
+      where: { id },
+      data: {
+        nickname,
+        saltedPassword: newSaltedPassword,
+      },
+    });
+
+    return result;
+  }
+}
