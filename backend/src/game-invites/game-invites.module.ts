7aac48dbdc4332b5c8f52d4eccbb912ae0e89f7f
knotekbr <knotekbr@gmail.com>
2023-12-04 18:41:13 -0500

    update module imports/exports

--- backend/src/game-invites/game-invites.module.ts
+++ backend/src/game-invites/game-invites.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { GameInvitesService } from "./game-invites.service";
 import { GameInvitesController } from "./game-invites.controller";
 import { GamesModule } from "src/games/games.module";
 
 @Module({
-  imports: [GamesModule],
+  imports: [forwardRef(() => GamesModule)],
   providers: [GameInvitesService],
   controllers: [GameInvitesController],
+  exports: [GameInvitesService],
 })
 export class GameInvitesModule {}
