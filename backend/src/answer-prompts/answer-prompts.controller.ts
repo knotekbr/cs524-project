a1a640f7b247b2e798c8893153f16ed28da51791
knotekbr <knotekbr@gmail.com>
2023-12-05 10:10:01 -0500

    add csv parser

---
+++ backend/src/answer-prompts/answer-prompts.controller.ts
@@ -0,0 +1,60 @@
+import { BadRequestException, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Readable } from "stream";
+import * as papa from "papaparse";
+import { ParsedPrompt } from "./interfaces/parsedPrompt";
+import { Auth } from "src/common/decorators/auth.decorator";
+import { Role } from "src/common/enums/role.enum";
+import { AnswerPromptsService } from "./answer-prompts.service";
+import { AnswerCategoriesService } from "src/answer-categories/answer-categories.service";
+import { AnswerCategory } from "@prisma/client";
+
+@Controller("answer-prompts")
+export class AnswerPromptsController {
+  constructor(
+    private answerPromptsService: AnswerPromptsService,
+    private answerCategoriesService: AnswerCategoriesService
+  ) {}
+
+  @Post("upload")
+  @Auth(Role.Admin)
+  @UseInterceptors(FileInterceptor("file", {}))
+  async uploadCsv(@UploadedFile() file: Express.Multer.File) {
+    if (!file.filename.endsWith(".csv")) {
+      throw new BadRequestException("Must be a csv file");
+    }
+
+    const fileStream = Readable.from(file.buffer);
+    papa.parse(fileStream, {
+      header: true,
+      worker: true,
+      delimiter: ",",
+      dynamicTyping: true,
+      complete: async (results: papa.ParseResult<ParsedPrompt>) => {
+        const { data } = results;
+        const categories: Record<string, AnswerCategory> = {};
+
+        for (const parsed of data) {
+          let category: AnswerCategory;
+
+          if (parsed.Category in categories) {
+            category = categories[parsed.Category];
+          } else {
+            category = await this.answerCategoriesService.create({
+              categoryName: parsed.Category,
+            });
+          }
+
+          await this.answerPromptsService.create({
+            category: { connect: { id: category.id } },
+            correctResponse: parsed["Correct Answer"],
+            difficulty: parsed.Difficulty,
+            prompt: parsed.Question,
+            otherResponses: [parsed["Wrong Answer 1"], parsed["Wrong Answer 2"], parsed["Wrong Answer 3"]],
+          });
+        }
+      },
+    });
+  }
+}
