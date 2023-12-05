44049e64ddaa8d4558be96d5dc5ef8a8116b272f
knotekbr <70414881+knotekbr@users.noreply.github.com>
2023-12-05 13:51:31 -0500

    heroku setup (#17)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors({ origin: "*" });
   app.useWebSocketAdapter(new AuthIoAdapter(app));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
